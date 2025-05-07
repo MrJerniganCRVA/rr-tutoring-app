3e9f025aab73096557d583b984382ab94518ed7c
MrJerniganCRVA <zachary.jernigan@coderva.org>
2025-05-07 11:40:58 -0400

    Create Teacher.js
    Adding models directory and Teacher.js from Claude.

---
+++ server/models/Teacher.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Teacher = sequelize.define('Teacher', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  subject: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = Teacher;
