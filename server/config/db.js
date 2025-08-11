b36b4a68853f3e2e28e1ffa2f0e60535b169c99c
MrJerniganCRVA <zachary.jernigan@coderva.org>
2025-08-11 10:58:51 -0400

    Adding .gitignore file for production

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,24 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
+let sequelize;
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgresql',
-  dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production' ? {
-      require: true,
-      rejectUnauthorized: false
-    } : false
-  }
-});
+
+if(process.env.DATABASE_URL){
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgresql',
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production' ? {
+        require: true,
+        rejectUnauthorized: false
+      } : false
+    }
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+    logging:false
+  });
+}
 
 module.exports = sequelize;
