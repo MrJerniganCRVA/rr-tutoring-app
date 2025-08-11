105d7f604e1d9f2b864cc408b244a874261ee8bd
Zachary Jernigan <zachary.jernigan@coderva.org>
2025-08-11 10:33:25 -0400

    Update db.js
    Need to use PostgreSQL.

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,14 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, '../database.sqlite'),
-  logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgresql',
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? {
+      require: true,
+      rejectUnauthorized: false
+    } : false
+  }
 });
 
 module.exports = sequelize;
